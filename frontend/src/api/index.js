76c9cf013feec7e219cf484f24a0a9564c82c004
Alejandro Delgado <ialexx9919@gmail.com>
2024-01-07 21:04:18 +0100

    vercel-config

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://multimedia-mern-app-v2l4.vercel.app",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
