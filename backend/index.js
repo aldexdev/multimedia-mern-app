d19c3465d7e7049d1ba01de6477626aa0578c2d4
Alejandro Delgado <ialexx9919@gmail.com>
2024-01-07 21:22:10 +0100

    vercel-config

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(
   cors({
-    origin: ["https://memories-tfg.vercel.app"],
+    origin: ["http://memories-tfg.vercel.app"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     CREDENTIALS: true,
   })
