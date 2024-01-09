9956a63d9ffac9ce7e79ed65f9e2ae6839f94390
Alejandro Delgado <ialexx9919@gmail.com>
2024-01-09 11:21:59 +0100

    changing-production

--- frontend/src/components/Cam/actions.js
+++ frontend/src/components/Cam/actions.js
@@ -10,7 +10,7 @@ export const actions = (gesture, change, posts, logout) => {
   switch (name) {
     // go to sign in
     case "thumbs_up":
-      window.location.href = "http://localhost:3000/auth";
+      window.location.href = "https://memories-tfg.vercel.app/auth";
       break;
 
     // logout
@@ -20,56 +20,56 @@ export const actions = (gesture, change, posts, logout) => {
 
     // go to main page
     case "palm":
-      window.location.href = "http://localhost:3000/posts";
+      window.location.href = "https://memories-tfg.vercel.app/posts";
       break;
 
     // go ahead to a page
     case "right":
       if (count === 0) {
-        window.location.href = "http://localhost:3000/posts?page=2";
+        window.location.href = "https://memories-tfg.vercel.app/posts?page=2";
       } else {
         count++;
-        window.location.href = `http://localhost:3000/posts?page=${count}`;
+        window.location.href = `https://memories-tfg.vercel.app/posts?page=${count}`;
       }
       break;
 
     // go back to a page
     case "left":
       if (count === 2) {
-        window.location.href = "http://localhost:3000/posts";
+        window.location.href = "https://memories-tfg.vercel.app/posts";
       } else if (count > 2) {
         count--;
-        window.location.href = `http://localhost:3000/posts?page=${count}`;
+        window.location.href = `https://memories-tfg.vercel.app/posts?page=${count}`;
       }
       break;
 
     // switch auth forms
     case "i_love_you":
-      if (window.location.href === "http://localhost:3000/auth") change();
+      if (window.location.href === "https://memories-tfg.vercel.app/auth") change();
       break;
 
     // view first post of the page post
     case "one":
       if (window.location.href.includes("/posts"))
-        window.location.href = `http://localhost:3000/posts/${posts[0]?._id}`;
+        window.location.href = `https://memories-tfg.vercel.app/posts/${posts[0]?._id}`;
       break;
 
     // view second post of the page post
     case "victory":
       if (window.location.href.includes("/posts"))
-        window.location.href = `http://localhost:3000/posts/${posts[1]?._id}`;
+        window.location.href = `https://memories-tfg.vercel.app/posts/${posts[1]?._id}`;
       break;
 
     // view third post of the page post
     case "three":
       if (window.location.href.includes("/posts"))
-        window.location.href = `http://localhost:3000/posts/${posts[2]?._id}`;
+        window.location.href = `https://memories-tfg.vercel.app/posts/${posts[2]?._id}`;
       break;
 
     // view forth post of the page post
     case "four":
       if (window.location.href.includes("/posts"))
-        window.location.href = `http://localhost:3000/posts/${posts[3]?._id}`;
+        window.location.href = `https://memories-tfg.vercel.app/posts/${posts[3]?._id}`;
       break;
 
     default:
